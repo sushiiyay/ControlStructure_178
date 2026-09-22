bc0d4bd9fd99d66795475d849e64aceba50a3f36
sushiiyay <nfa.aliya7811@gmail.com>
2026-09-22 11:21:36 +0700

    masukkan nilai fibonacci

--- controlstructuredua.py
+++ controlstructuredua.py
@@ -1,5 +1,15 @@
-angka 1 = int(input("Masukkan angka pertama: "))
-angka 2 = int(input("Masukkan angka kedua: "))
-angka 3 = int(input("Masukkan angka ketiga: "))
+angka_a = float(input("Masukkan angka_a: "))
+angka_b = float(input("Masukkan angka_b: "))
+angka_c = float(input("Masukkan angka_c: "))
 
-if angka
+if angka_a > angka_b and angka_b > angka_c:
+    terbesar = a
+    print("Angka terbesar adalah:", terbesar)
+elif angka_b > angka_a and angka_b > angka_c:
+    terbesar = b
+    print("Angka terbesar adalah:", terbesar)
+elif angka_c > angka_a and angka_c > angka_b:
+    terbesar = c
+    print("Angka terbesar adalah:", terbesar)
+else:
+    print("Tidak ada angka terbesar")
---
+++ controlstructuretiga.py
@@ -0,0 +1,4 @@
+nilai = int(input("Masukkan nilai fibonacci"))
+a = 0
+b = 1
+
